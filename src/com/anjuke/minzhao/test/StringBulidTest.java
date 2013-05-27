f36244a10de031ba7dd23eae15a4f51d85386518
minzhao <minzhao@anjuke.com>
2013-05-27 18:41:13 +0800

    新增字符串

---
+++ src/com/anjuke/minzhao/test/StringBulidTest.java
@@ -0,0 +1,27 @@
+package com.anjuke.minzhao.test;
+import java.util.*;
+import java.util.regex.*;
+
+public class StringBulidTest {
+
+
+public static void main(String args[]) {
+	
+    //String str = "title=d\rdescription1=e\rdescription2=f"; 
+    class Weixueyuan{
+    	String title=new String("d");
+    	String description1 = "e";
+    	String des = "f";
+    	public void say(){
+            System.out.println("title的值"+ title+"");
+            System.out.println("description1的值"+ description1+"");
+            //System.out.println("description2的值"+ des);
+            System.out.println("");）
+        }
+    }
+    Weixueyuan obj1=new Weixueyuan();  //创建对象的同时进行初始化
+    obj1.say();
+    
+
+}
+}
