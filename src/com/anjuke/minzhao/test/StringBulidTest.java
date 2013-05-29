38b0829d362d513e55bcb05187b1cd22a62bb7e4
minzhao <minzhao@anjuke.com>
2013-05-29 14:11:10 +0800

    利用了字符串中的replace 和 startwith方法

--- src/com/anjuke/minzhao/test/StringBulidTest.java
+++ src/com/anjuke/minzhao/test/StringBulidTest.java
@@ -1,27 +1,60 @@
 package com.anjuke.minzhao.test;
-import java.util.*;
-import java.util.regex.*;
-
+/*想到的最搓的方法*/
 public class StringBulidTest {
-
-
-public static void main(String args[]) {
 	
-    //String str = "title=d\rdescription1=e\rdescription2=f"; 
-    class Weixueyuan{
-    	String title=new String("d");
-    	String description1 = "e";
-    	String des = "f";
-    	public void say(){
-            System.out.println("title的值"+ title+"");
-            System.out.println("description1的值"+ description1+"");
-            System.out.println("description2的值"+ des);
-            System.out.println("");
-        }
-    }
-    Weixueyuan obj1=new Weixueyuan();  //创建对象的同时进行初始化
-    obj1.say();
-    
+	public static String title = null;
+	public static String description1 = null;
+	public static String description2 = null;
+	public static final String TITLE = "title=";
+	public static final String DESCRIPTION1 = "description1=";
+	public static final String DESCRIPTION2 = "description2=";
+	public static final String NULL = null;
+	
+	public static void main(String args[]) {
+	    String str = "title=d\rdescription1=e\rdescription2=f"; 
+	    String str1 = "description1=e\rdescription2=f\rtitle=d";
+	    String str2 = "description1=e\rdescription2=f";
+	    String str3 ="title=d\rdescription1=e\rdescription2=f\rread=s";
+	    
+	    test ss = new StringBulidTest().new test();
+		ss.tranfrom(str1);
+	}
+	public class test {
+		
+		public void tranfrom(String str) {
+			if(str==null) {
+				return;
+			}
+			else{
+				String test1[] = str.split("\r");
+			
+				for(int i=0; i<test1.length; i++) {
+					if(test1[i].startsWith(TITLE)){
+						title = test1[i].replace(TITLE, "");
+					}
+					else if(test1[i].startsWith(DESCRIPTION1)) {
+						description1 = test1[i].replace(DESCRIPTION1, "");
+						
+					}
+					else if(test1[i].startsWith(DESCRIPTION2)) {
+						description2 = test1[i].replace(DESCRIPTION2, "");
+					}
+					else {
+						test1[i]=NULL;
+					}
+				}
+				
+				System.out.println("title="+title);
+				System.out.println("description1="+description1);
+				System.out.println("description2="+description2);
+			}
+			
+		}
+	}
+}
 
-}
-}
+
+
+
+
+
