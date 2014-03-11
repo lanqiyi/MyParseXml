1fd2325e0056aa3e7bcf3b6d236d91b5966ae6f3
snakecon <snakecon@gmail.com>
2014-03-11 23:23:56 +0800

    Add csv file processing

--- src/com/anjuke/minzhao/ParseUri/rs.py
+++ src/com/anjuke/minzhao/ParseUri/rs.py
@@ -1,24 +1,41 @@
-import urllib2, urllib
+import urllib2, urllib, csv
 
 class RedirectHandler(urllib2.HTTPRedirectHandler):
-    def http_erroe_302(self, req, fp, code, msg, headers):
-        result = urllib2.HTTPRedirectHandler.http_error_301(self,req,fp,code,msg,headers)
+    def http_error_301(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)
         result.status = code
         return result
+
     def http_error_302(self, req, fp, code, msg, headers):
-        result = urllib2.HTTPRedirectHandler.http_error_302(self,req,fp,code,msg,headers)
+        result = urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
         result.status = code
         return result
-input_url ="http://shanghai.anjuke.com/community/props/sale/3536"
-data={"name":"hank", "passwd":"hjz"}
-headers={"User-Agent": "JUC (Linux; U; 4.0.3; zh-cn; U18GT; 480*764) UCWEB1.0.1.113/147/800"}
-opener = urllib2.build_opener(RedirectHandler)
-urllib2.install_opener(opener)
-request = urllib2.Request(input_url,urllib.urlencode(data),headers)
-response = urllib2.urlopen(request)
-#print response.status
-print response.geturl()
-if 'touchweb' in response.read():
-    print 'touchweb'
-else:
-    print 'pc'
+
+
+def parseResponse(input_url):
+    try:
+        data = {"name": "hank", "passwd": "hjz"}
+        headers = {"User-Agent": "JUC (Linux; U; 4.0.3; zh-cn; U18GT; 480*764) UCWEB1.0.1.113/147/800"}
+        opener = urllib2.build_opener(RedirectHandler)
+        urllib2.install_opener(opener)
+        request = urllib2.Request(input_url, urllib.urlencode(data), headers)
+        response = urllib2.urlopen(request)
+        #print response.status
+
+        print response.geturl()
+        if 'touchweb' in response.read():
+            return 'touchweb'
+        else:
+            return 'pc'
+    except:
+        return 'error'
+
+# This is a demo
+print parseResponse("http://shanghai.anjuke.com/")
+
+# read and parse file
+reader = csv.reader(file('Match_Regex_Demo_url.csv', 'rb'))
+writer = csv.writer(file('Match_Regex_Demo_Url_Result.csv', 'wb'))
+for line in reader:
+    if line[1].startswith('http'):
+        writer.writerow([line[0], line[1], line[2], parseResponse(line[1])])
